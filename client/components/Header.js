1035373585e49852963e6d7f3a5937dd48215264
Francisco Diaz <franciscojavier.diaz@i3television.es>
2018-02-02 08:50:04 +0100

    Added logout mutation

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
-import query from '../queries/currentUser';
+import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
+    onLogoutClick() {
+        this.props.mutate({
+            refetchQueries: [{ query }]
+        });
+    }
     renderButtons() {
         const { loading, user } = this.props.data;
         if (loading) {
             return (<div>Loading...</div>);
         }
         if (user) {
-            return (<div>Logout</div>)
+            return (
+                <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>
+            )
         } else {
             return (
                 <div>
@@ -40,4 +48,6 @@ class Header extends Component {
     }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+    graphql(query)(Header)
+);
